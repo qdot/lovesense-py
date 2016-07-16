0c59523f8d9c677fb0e38cbe09250a93fc757162
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-16 15:42:26 -0700

    chore: Start python 2/3'ing things

--- examples/get_device_info.py
+++ examples/get_device_info.py
@@ -1,17 +1,18 @@
 from liblovense.comm import LovenseSyncSerial
 
+
 def main():
     s = LovenseSyncSerial()
     s.open("/dev/tty.LVS-C011-SPPDev")
     s.deviceType()
     status = s.readStatus()
-    print "Device Type: %s" % (status)
+    print("Device Type: {}".format(status))
     s.deviceStatus()
     status = s.readStatus()
-    print "Device Type: %s" % (status)
+    print("Device Status: {}".format(status))
     s.batteryLevel()
     status = s.readStatus()
-    print "Battery Level: %s" % (status)
+    print("Battery Level: {}".format(status))
     return 0
 
 if __name__ == "__main__":
